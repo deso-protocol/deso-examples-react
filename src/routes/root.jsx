4abfdf739b38b3318d524aa713b85bebc1d196a1
diamondhands <diamondhands@bitcloutdev.com>
2023-01-31 17:33:56 -0800

    Refactoring to work with new login() assumptions

--- src/routes/root.jsx
+++ src/routes/root.jsx
@@ -12,81 +12,92 @@ export const Root = () => {
   });
 
   useEffect(
-    () =>
-      // Typically, you will want access to the current user throughout your
-      // app. Here we subscribe to identity at the root of our app so that we
-      // can access the current user in any component and our components will
-      // re-render when the current user changes. We can access the current
-      // user via the useContext hook from anywhere in our app.
-      //
-      // example:
-      //    const { user } = useContext(UserContext);
-      //
-      // See src/components/nav.jsx for an concrete use case.
-      //
-      // NOTE: This function could be chatty. You might want to implement some
-      // caching or memoization to reduce unnecessary network calls. We have not
-      // done so here for simplicity and to reduce noise from the example.
-      identity.subscribe(({ currentUser, alternateUsers }) => {
-        // NOTE: You can use this callback to update your app state in any way you want.
-        // Here we just use a simple useState hook combined with react context. You can
-        // use redux, mobx, or any other state management library you want.
+    () => {
+        identity.configure({
+            // identityURI: "http://localhost:4201",
+            spendingLimitOptions: {
+                GlobalDESOLimit: 10000000, // 0.01 DESO
+                TransactionCountLimitMap: {
+                    SUBMIT_POST: 3,
+                },
+            },
+        });
 
-        // If your app supports multiple accounts for a user and the current user logs out,
-        // you can choose a fallback user to use as the current user from the alternateUsers
-        // object. This is a choice you can make depending on the requirements of your own app.
-        // Here we just choose the first alternate user as the fallback user. Alternate users
-        // are all users that have been logged in to your app and never logged out.
-        if (alternateUsers && !currentUser) {
-          const fallbackUser = Object.values(alternateUsers)[0];
-          identity.setActiveUser(fallbackUser.publicKey);
-          // NOTE: setting the active user will trigger a new state change in
-          // identity which will re-trigger this callback so we just return
-          // here.
-          return;
-        }
+        // Typically, you will want access to the current user throughout your
+        // app. Here we subscribe to identity at the root of our app so that we
+        // can access the current user in any component and our components will
+        // re-render when the current user changes. We can access the current
+        // user via the useContext hook from anywhere in our app.
+        //
+        // example:
+        //    const { user } = useContext(UserContext);
+        //
+        // See src/components/nav.jsx for an concrete use case.
+        //
+        // NOTE: This function could be chatty. You might want to implement some
+        // caching or memoization to reduce unnecessary network calls. We have not
+        // done so here for simplicity and to reduce noise from the example.
+        identity.subscribe(({currentUser, alternateUsers}) => {
+            // NOTE: You can use this callback to update your app state in any way you want.
+            // Here we just use a simple useState hook combined with react context. You can
+            // use redux, mobx, or any other state management library you want.
 
-        if (!currentUser) {
-          // if no user is logged in or the user has logged out, set our app user state to null
-          // All of our components will re-render and update accordingly
-          setUserState((state) => ({
-            ...state,
-            currentUser: null,
-            isLoading: false,
-          }));
-        } else if (
-          currentUser?.publicKey !== userState.currentUser?.PublicKeyBase58Check
-        ) {
-          // if the user is logged in, fetch the user's details from a node and set the app user state
-          // All of our components will re-render and update accordingly. We also fetch any alternate users
-          // we may have stored in local storage.
-          const alternateUserKeys =
-            Object.values(alternateUsers ?? {})?.map((u) => u.publicKey) ?? [];
+            // If your app supports multiple accounts for a user and the current user logs out,
+            // you can choose a fallback user to use as the current user from the alternateUsers
+            // object. This is a choice you can make depending on the requirements of your own app.
+            // Here we just choose the first alternate user as the fallback user. Alternate users
+            // are all users that have been logged in to your app and never logged out.
+            if (alternateUsers && !currentUser) {
+                const fallbackUser = Object.values(alternateUsers)[0];
+                identity.setActiveUser(fallbackUser.publicKey);
+                // NOTE: setting the active user will trigger a new state change in
+                // identity which will re-trigger this callback so we just return
+                // here.
+                return;
+            }
 
-          // We set isLoading to true so that we can show a loading indicator wherever
-          // we reference the user state in our app.
-          setUserState((state) => ({
-            ...state,
-            isLoading: true,
-          }));
+            if (!currentUser) {
+                // if no user is logged in or the user has logged out, set our app user state to null
+                // All of our components will re-render and update accordingly
+                setUserState((state) => ({
+                    ...state,
+                    currentUser: null,
+                    isLoading: false,
+                }));
+            } else if (
+                currentUser?.publicKey !== userState.currentUser?.PublicKeyBase58Check
+            ) {
+                // if the user is logged in, fetch the user's details from a node and set the app user state
+                // All of our components will re-render and update accordingly. We also fetch any alternate users
+                // we may have stored in local storage.
+                const alternateUserKeys =
+                    Object.values(alternateUsers ?? {})?.map((u) => u.publicKey) ?? [];
 
-          fetchUsers([currentUser.publicKey, ...alternateUserKeys])
-            .then((userList) => {
-              const [currentUser, ...alternateUsers] = userList;
-              setUserState((state) => ({
-                ...state,
-                currentUser,
-                alternateUsers,
-              }));
-            })
-            .finally(() =>
-              setUserState((state) => ({
-                ...state,
-                isLoading: false,
-              }))
-            );
-        }
-      }),
+                // We set isLoading to true so that we can show a loading indicator wherever
+                // we reference the user state in our app.
+                setUserState((state) => ({
+                    ...state,
+                    isLoading: true,
+                }));
+
+                fetchUsers([currentUser.publicKey, ...alternateUserKeys])
+                    .then((userList) => {
+                        const [currentUser, ...alternateUsers] = userList;
+                        setUserState((state) => ({
+                            ...state,
+                            currentUser,
+                            alternateUsers,
+                        }));
+                    })
+                    .finally(() =>
+                        setUserState((state) => ({
+                            ...state,
+                            isLoading: false,
+                        }))
+                    );
+            }
+        })
+    },
     [] /* NOTE: We pass an empty array to useEffect so that it only runs once for our entire app session */
   );
 
--- src/routes/sign-and-submit-tx.jsx
+++ src/routes/sign-and-submit-tx.jsx
@@ -2,77 +2,94 @@ import { identity } from "@deso-core/identity";
 import { useContext } from "react";
 import { UserContext } from "../contexts";
 
+// FIXME: Import this from @deso-core/identity instead of re-defining it.
+const NO_MONEY_ERROR = 'User does not have sufficient funds in their wallet to complete the transaction';
+
 export const SignAndSubmitTx = () => {
   const { currentUser: user } = useContext(UserContext);
-  const username =
+  const usernameOrPubkey =
     user?.ProfileEntryResponse?.Username ?? user?.PublicKeyBase58Check;
 
-  if (!username) {
+  let hasPostingPermissions = identity.hasPermissions({
+      TransactionCountLimitMap: {
+          SUBMIT_POST: 1,
+      },
+  })
+  if (!usernameOrPubkey || !user.BalanceNanos || !hasPostingPermissions) {
     return (
-      <button onClick={() => identity.login()}>Login to create a post</button>
+      <button onClick={() => {
+          identity.login({
+              getFreeDeso: true,
+          }).then((res)=>{
+              alert('Login success!')
+          }).catch((err)=>{
+              if (err?.toString().indexOf(NO_MONEY_ERROR) >= 0) {
+                  alert('You need DESO in order to post!')
+              } else {
+                  alert(err)
+              }
+          })
+      }}>Login to create a post</button>
     );
-  }
+  } else {
+      return (
+          <>
+              <h1>Submit a signed post transaction</h1>
+              <form
+                  onSubmit={(e) => {
+                      e.preventDefault();
 
-  return (
-    <>
-      <h1>Submit a signed post transaction</h1>
-      <form
-        onSubmit={(e) => {
-          e.preventDefault();
+                      // check if the user can make a post
+                      if (!hasPostingPermissions) {
+                          // if the user doesn't have permissions, request them
+                          identity.requestPermissions({
+                              GlobalDESOLimit: 10000000, // 0.01 DESO
+                              TransactionCountLimitMap: {
+                                  SUBMIT_POST: 3,
+                              },
+                          }).then((res)=>{
+                              console.log(res)
+                              debugger;
+                          });
+                          return;
+                      }
 
-          // check if the user can make a post
-          if (
-            !identity.hasPermissions({
-              TransactionCountLimitMap: {
-                SUBMIT_POST: 1,
-              },
-            })
-          ) {
-            // if the user doesn't have permissions, request them
-            identity.requestPermissions({
-              GlobalDESOLimit: 10000000, // 0.01 DESO
-              TransactionCountLimitMap: {
-                SUBMIT_POST: 3,
-              },
-            });
-            return;
-          }
+                      const body = e.target[0].value;
 
-          const body = e.target[0].value;
+                      const createTx = () =>
+                          fetch("https://node.deso.org/api/v0/submit-post", {
+                              method: "POST",
+                              headers: { "Content-Type": "application/json" },
+                              body: JSON.stringify({
+                                  UpdaterPublicKeyBase58Check: user?.PublicKeyBase58Check,
+                                  BodyObj: {
+                                      Body: body,
+                                      ImageURLs: [],
+                                      VideoURLs: [],
+                                  },
+                                  MinFeeRateNanosPerKB: 1000,
+                              }),
+                          }).then((res) => res.json());
 
-          const createTx = () =>
-            fetch("https://node.deso.org/api/v0/submit-post", {
-              method: "POST",
-              headers: { "Content-Type": "application/json" },
-              body: JSON.stringify({
-                UpdaterPublicKeyBase58Check: identity.activePublicKey,
-                BodyObj: {
-                  Body: body,
-                  ImageURLs: [],
-                  VideoURLs: [],
-                },
-                MinFeeRateNanosPerKB: 1000,
-              }),
-            }).then((res) => res.json());
-
-          identity
-            .signAndSubmitTx(createTx)
-            .then(() => alert("Post submitted successfully!"))
-            .catch((e) =>
-              alert(`Error submitting post. Please try again. ${e.toString()}`)
-            );
-        }}
-      >
+                      identity
+                          .signAndSubmitTx(createTx)
+                          .then(() => alert("Post submitted successfully!"))
+                          .catch((e) =>
+                              alert(`Error submitting post. Please try again. ${e.toString()}`)
+                          );
+                  }}
+              >
         <textarea
-          name="post-textarea"
-          cols={30}
-          rows={10}
-          style={{ border: "1px solid black" }}
+            name="post-textarea"
+            cols={30}
+            rows={10}
+            style={{ border: "1px solid black" }}
         ></textarea>
-        <div>
-          <button>Post</button>
-        </div>
-      </form>
-    </>
-  );
+                  <div>
+                      <button>Post</button>
+                  </div>
+              </form>
+          </>
+      );
+  }
 };
