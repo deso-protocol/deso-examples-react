2315afc7ba752b6d1c54786d2573f7b24e6ffcc5
gage <87737875+GageVanK@users.noreply.github.com>
2023-05-16 01:45:17 +0000

    Update MantineHeader.jsx

--- src/components/AppShell/MantineHeader.jsx
+++ src/components/AppShell/MantineHeader.jsx
@@ -8,9 +8,7 @@ import {
   Header,
   Group,
   Button,
-  UnstyledButton,
   Text,
-  ThemeIcon,
   Divider,
   Box,
   Burger,
@@ -98,41 +96,8 @@ const useStyles = createStyles((theme) => ({
   },
 }));
 
-const mockdata = [
-  {
-    icon: IconCode,
-    title: "Open source",
-    description: "This Pokémon’s cry is very loud and distracting",
-  },
-  {
-    icon: IconCoin,
-    title: "Free for everyone",
-    description: "The fluid of Smeargle’s tail secretions changes",
-  },
-  {
-    icon: IconBook,
-    title: "Documentation",
-    description: "Yanma is capable of seeing 360 degrees without",
-  },
-  {
-    icon: IconFingerprint,
-    title: "Security",
-    description: "The shell’s rounded shape and the grooves on its.",
-  },
-  {
-    icon: IconChartPie3,
-    title: "Analytics",
-    description: "This Pokémon uses its flying ability to quickly chase",
-  },
-  {
-    icon: IconNotification,
-    title: "Notifications",
-    description: "Combusken battles with the intensely hot flames it spews",
-  },
-];
-
 export const MantineHeader = () => {
-  const { currentUser, isLoading } = useContext(UserContext);
+  const { isLoading } = useContext(UserContext);
   const [drawerOpened, { toggle: toggleDrawer, close: closeDrawer }] =
     useDisclosure(false);
 
