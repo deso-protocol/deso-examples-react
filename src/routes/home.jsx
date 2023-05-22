8e99a6f63a90a188cfb362a88b1a10765dde839a
gage <87737875+GageVanK@users.noreply.github.com>
2023-05-22 22:44:50 +0000

    Update home.jsx

--- src/routes/home.jsx
+++ src/routes/home.jsx
@@ -1,12 +1,22 @@
 import { Welcome } from "../components/Welcome";
 import { SignAndSubmitTx } from "../routes/sign-and-submit-tx";
-import { Space, Tabs, Divider } from "@mantine/core";
+import {
+  Space,
+  Tabs,
+  Divider,
+  Text,
+  UnstyledButton,
+  Center,
+  Paper,
+} from "@mantine/core";
 import { HotFeed } from "../components/HotFeed";
 import { FollowerFeed } from "../components/FollowerFeed";
 import { useState } from "react";
+import { GiWaveCrest } from "react-icons/gi";
+import { IconCampfire, IconUsers } from "@tabler/icons-react";
 
 export const Home = () => {
-  const [activeTab, setActiveTab] = useState("first");
+  const [activeTab, setActiveTab] = useState("third");
 
   return (
     <>
@@ -24,14 +34,45 @@ export const Home = () => {
         onTabChange={setActiveTab}
       >
         <Tabs.List position="center">
-          <Tabs.Tab value="first">Hot Feed</Tabs.Tab>
-          <Tabs.Tab value="second">Follower Feed</Tabs.Tab>
+          <Tabs.Tab value="first">
+            <Center>
+              <GiWaveCrest size="1.2rem" />
+            </Center>
+            <Text fz="sm">Waves</Text>
+          </Tabs.Tab>
+          <Tabs.Tab value="second">
+            <Center>
+              <IconUsers />
+            </Center>
+            <Text fz="sm">Follower Feed</Text>
+          </Tabs.Tab>
+          <Tabs.Tab value="third">
+            <Center>
+              <IconCampfire />
+            </Center>
+            <Text fz="sm">Hot Feed</Text>
+          </Tabs.Tab>
         </Tabs.List>
 
         <Tabs.Panel value="first">
-          <HotFeed />
+          <Space h={222} />
+          <Center>
+            <Paper shadow="xl" radius="lg" p="xl" withBorder>
+              <Text
+                size="xl"
+                lineClamp={4}
+                variant="gradient"
+                gradient={{ from: "indigo", to: "cyan", deg: 45 }}
+              >
+                Coming soon
+              </Text>
+            </Paper>
+          </Center>
         </Tabs.Panel>
         <Tabs.Panel value="second">
+          <HotFeed />
+        </Tabs.Panel>
+        <Tabs.Panel value="third">
           <FollowerFeed />
         </Tabs.Panel>
       </Tabs>
