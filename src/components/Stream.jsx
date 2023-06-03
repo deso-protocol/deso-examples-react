51bd7f6717d20e08a17b514e1d38d5f6d87187ba
gage <87737875+GageVanK@users.noreply.github.com>
2023-06-03 22:47:44 +0000

    Update Stream.jsx

--- src/components/Stream.jsx
+++ src/components/Stream.jsx
@@ -1,5 +1,12 @@
-import { Player, useCreateStream, useEndStream } from "@livepeer/react";
+import {
+  Player,
+  useCreateStream,
+  useEndStream,
+  useUpdateStream,
+  updateStream,
+} from "@livepeer/react";
 import { useMemo, useState, useEffect, useContext } from "react";
+import { useDisclosure } from "@mantine/hooks";
 import {
   Paper,
   Textarea,
@@ -13,6 +20,7 @@ import {
   Card,
   Badge,
   Loader,
+  LoadingOverlay,
 } from "@mantine/core";
 import { IconCopy, IconCheck } from "@tabler/icons-react";
 import { updateProfile } from "deso-protocol";
@@ -21,116 +29,144 @@ import { DeSoIdentityContext } from "react-deso-protocol";
 export const Stream = () => {
   const { currentUser } = useContext(DeSoIdentityContext);
   const [streamName, setStreamName] = useState("");
+ const [disable, { toggle }] = useDisclosure(false);
   const {
     mutate: createStream,
     data: stream,
     status,
   } = useCreateStream(streamName ? { name: streamName } : null);
-
+  console.log(stream);
   const isLoading = useMemo(() => status === "loading", [status]);
+
+  const streamId = stream?.id;
+  const { mutate: updateStream } = useUpdateStream({
+    streamId,
+    suspend: true,
+  });
+
+  const handleEndStream = () => {
+    updateStream?.();
+    setStreamName("");
+   
+  };
+
+  const handleCreateStream = () => {
+    createStream?.();
+    toggle();
+  };
 
   return (
     <Paper shadow="sm" p="lg" withBorder>
-      <Textarea
-        placeholder="Enter Stream Title"
-        variant="filled"
-        radius="md"
-        onChange={(e) => setStreamName(e.target.value)}
-      />
-      <Space h="xl" />
-      {stream &&
-        stream.rtmpIngestUrl &&
-        (!stream?.playbackUrl || !stream.isActive) && (
-          <Center>
-            <Card shadow="sm" p="lg" radius="md" withBorder>
-              <Group position="center">
-                <h4 lineClamp={1}>Stream Server:</h4>
+      <>
+        <Textarea
+          placeholder="Enter Stream Title"
+          variant="filled"
+          radius="md"
+          disabled={disable}
+          onChange={(e) => setStreamName(e.target.value)}
+        />
+        <Space h="xl" />
+      </>
 
-                <CopyButton
-                  value="rtmp://rtmp.livepeer.com/live"
-                  timeout={2000}
-                >
-                  {({ copied, copy }) => (
-                    <Tooltip
-                      position="top"
-                      label={copied ? "Copied" : "Stream Server"}
-                      withArrow
+      {status === "success" && (
+        <>
+          {streamName ? (
+            <>
+              <Center>
+                <Card shadow="sm" p="lg" radius="md" withBorder>
+                  <Group position="center">
+                    <h4 lineClamp={1}>Stream Server:</h4>
+                    <CopyButton
+                      value="rtmp://rtmp.livepeer.com/live"
+                      timeout={2000}
                     >
-                      <ActionIcon
-                        color={copied ? "teal" : "gray"}
-                        onClick={copy}
-                      >
-                        {copied ? (
-                          <IconCheck size={16} />
-                        ) : (
-                          <IconCopy size={16} />
-                        )}
-                      </ActionIcon>
-                    </Tooltip>
-                  )}
-                </CopyButton>
-              </Group>
+                      {({ copied, copy }) => (
+                        <Tooltip
+                          position="top"
+                          label={copied ? "Copied" : "Stream Server"}
+                          withArrow
+                        >
+                          <ActionIcon
+                            color={copied ? "teal" : "gray"}
+                            onClick={copy}
+                          >
+                            {copied ? (
+                              <IconCheck size={16} />
+                            ) : (
+                              <IconCopy size={16} />
+                            )}
+                          </ActionIcon>
+                        </Tooltip>
+                      )}
+                    </CopyButton>
+                  </Group>
 
-              <Group position="center">
-                <h4>Stream Key:</h4>
-
-                <CopyButton value={stream.streamKey} timeout={2000}>
-                  {({ copied, copy }) => (
-                    <Tooltip
-                      position="top"
-                      label={copied ? "Copied" : "Stream Key"}
-                      withArrow
-                    >
-                      <ActionIcon
-                        color={copied ? "teal" : "gray"}
-                        onClick={copy}
-                      >
-                        {copied ? (
-                          <IconCheck size={16} />
-                        ) : (
-                          <IconCopy size={16} />
-                        )}
-                      </ActionIcon>
-                    </Tooltip>
-                  )}
-                </CopyButton>
-              </Group>
-
-              <Group position="center">
-                <Badge radius="sm" size="xl">
-                  {streamName}
-                </Badge>
-              </Group>
+                  <Group position="center">
+                    <h4>Stream Key:</h4>
+                    <CopyButton value={stream.streamKey} timeout={2000}>
+                      {({ copied, copy }) => (
+                        <Tooltip
+                          position="top"
+                          label={copied ? "Copied" : "Stream Key"}
+                          withArrow
+                        >
+                          <ActionIcon
+                            color={copied ? "teal" : "gray"}
+                            onClick={copy}
+                          >
+                            {copied ? (
+                              <IconCheck size={16} />
+                            ) : (
+                              <IconCopy size={16} />
+                            )}
+                          </ActionIcon>
+                        </Tooltip>
+                      )}
+                    </CopyButton>
+                  </Group>
+                  <Space h="md" />
+                  <Group position="center">
+                    <Badge radius="sm" size="xl">
+                      {streamName}
+                    </Badge>
+                  </Group>
+                  <Space h="md" />
+                </Card>
+                <Space h="xl" />
+              </Center>
               <Space h="md" />
-            </Card>
-          </Center>
-        )}
-      <Space h="xl" />
+              <Group position="center">
+                
+                <Player
+                  title={stream?.name}
+                  playbackId={stream?.playbackId}
+                  autoPlay
+                  muted
+                />
+              </Group>
 
-      {stream?.playbackId && (
-        <Group position="center">
-          <Player
-            title={stream?.name}
-            playbackId={stream?.playbackId}
-            autoPlay
-            muted
-          />
-        </Group>
+              <Space h="md" />
+              <Group position="center">
+                <Button radius="xl" onClick={handleEndStream}>
+                  End Stream
+                </Button>
+              </Group>
+            </>
+          ) : (
+            <Group position="center">
+              <p>Stream suspended. Refresh to create a new stream.</p>
+            </Group>
+          )}
+        </>
       )}
 
-      {stream?.isLoading && (
+      {status === "loading" && (
         <Group position="center">
           <Loader size="sm" />
         </Group>
       )}
 
-      {stream?.isIdle && (
-        <Group position="center">
-          <p>Stream is idle.</p>
-        </Group>
-      )}
-
-      {stream?.isError && (
+      {status === "error" && (
         <Group position="center">
           <p>Error occurred while creating the stream.</p>
         </Group>
@@ -139,11 +175,7 @@ export const Stream = () => {
       <Space h="md" />
       {!stream && (
         <Group position="center">
-          <Button
-            radius="xl"
-            disabled={status === "loading" || !createStream}
-            onClick={() => createStream?.()}
-          >
+          <Button radius="xl" onClick={handleCreateStream}>
             Create Stream
           </Button>
         </Group>
