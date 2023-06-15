61795dbbf33572082114f70320c2c0b194f52c9c
gage <87737875+GageVanK@users.noreply.github.com>
2023-06-15 18:59:34 +0000

    Update wallet.jsx

--- src/routes/wallet.jsx
+++ src/routes/wallet.jsx
@@ -1,5 +1,9 @@
 import { Space, Center, Paper, Text, Divider } from "@mantine/core";
+import { DeSoIdentityContext } from "react-deso-protocol";
+import { useContext } from "react";
 export const Wallet = () => {
+  const { currentUser, isLoading } = useContext(DeSoIdentityContext);
+
   return (
     <>
       <Divider
@@ -15,21 +19,41 @@ export const Wallet = () => {
       />
       <Space h="xl" />
       <div>
-        <iframe
-          width="100%"
-          style={{
-            border: "none",
-            margin: "0px",
-            padding: "0px",
-            display: "flex",
-            overflow: "hidden",
-            borderRadius: "22px",
-            boxSizing: "border-box",
-            height: "auto",
-            minHeight: "100vh",
-          }}
-          src="https://heroswap.com/widget?affiliateAddress=BC1YLfjx3jKZeoShqr2r3QttepoYmvJGEs7vbYx1WYoNmNW9FY5VUu6"
-        />
+        {currentUser ? (
+          <iframe
+            title="heroswap"
+            width="100%"
+            style={{
+              border: "none",
+              margin: "0px",
+              padding: "0px",
+              display: "flex",
+              overflow: "hidden",
+              borderRadius: "22px",
+              boxSizing: "border-box",
+              height: "auto",
+              minHeight: "100vh",
+            }}
+            src={`https://heroswap.com/widget?affiliateAddress=${currentUser.PublicKeyBase58Check}`}
+          />
+        ) : (
+          <iframe
+            title="heroswap"
+            width="100%"
+            style={{
+              border: "none",
+              margin: "0px",
+              padding: "0px",
+              display: "flex",
+              overflow: "hidden",
+              borderRadius: "22px",
+              boxSizing: "border-box",
+              height: "auto",
+              minHeight: "100vh",
+            }}
+            src="https://heroswap.com/widget?affiliateAddress=BC1YLfjx3jKZeoShqr2r3QttepoYmvJGEs7vbYx1WYoNmNW9FY5VUu6"
+          />
+        )}
       </div>
       <Space h={222} />
     </>
